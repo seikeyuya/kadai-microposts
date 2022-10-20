66cee5f8cb2d6ba05684e880476853d01bacf708
yuuya.seike <wseikeyuya1@gmail.com>
2022-10-20 10:09:27 +0000

    再提出の修正

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,5 +1,5 @@
 
-@if (Auth::id() != $micropost->id)
+
 
     @if (Auth::user()->is_favoriting($micropost->id))
         {{-- unfavoriteボタンのフォーム --}}
@@ -12,19 +12,5 @@
             {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
-@endif
 
-@if (Auth::id() != $micropost->user_id)
 
-    @if (Auth::user()->is_favoriting($micropost->id))
-        {{-- unfavoriteボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
-        {!! Form::close() !!}
-    @else
-        {{-- favoriteボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
-        {!! Form::close() !!}
-    @endif
-@endif
